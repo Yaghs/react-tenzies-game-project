1b6a826a78de9b60f8ae8568f851f16a2466bcde
Yaghs <syaghinloo@gmail.com>
2026-01-10 19:21:46 -0500

    create a die component passing a number prop
    added styling to the die buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import Die from "./components/Die.js"
 function App() {
   return (
-    <main></main>
+    <main>
+      <div className="dice-container">
+        <Die number={1}/>
+        <Die number={2}/>
+        <Die number={3}/>
+        <Die number={4}/>
+        <Die number={5}/>
+        <Die number={6}/>
+        <Die number={7}/>
+        <Die number={8}/>
+        <Die number={9}/>
+        <Die number={10}/>
+      </div>
+      
+    </main>
   );
 }
 
